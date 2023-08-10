c0662e3da9c7d5c1398157cc01bb00d68ac91d1b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-10 23:58:37 +0530

    completed row file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Row from './Row'
-import requests from './requests';
+import requests from './requests.js';
 
 function App() {
   return (
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-const base_url = 'https://api.themoviedb.org/3';
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
@@ -9,7 +9,37 @@ function Row({title, fetchUrl, isLargeRow}) {
     // Fetch data and store it in movies state variable
     useEffect(() => {
 
-    });
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+
+            setMovies(request.data.results);
+        }
+        fetchData();
+
+    }, [fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row_posters">
+                {movies.map(movie => {
+                    return (
+                        <img 
+                            key={movie.id}
+                            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            alt={movie.name}
+                        />
+                    )
+                })}
+            </div>
+        </div>
+    );
+
+
+
+
 }
 
 export default Row;
