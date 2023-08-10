6915af8392429f5e33ffccac9790bd6c42c94db8
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-10 23:02:21 +0530

    code change

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,7 @@
 const API_KEY = 'f0c1a401a3bddb5c41f2e041f23efd24'
 
 const requests = {
+
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
 	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
 	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -9,4 +10,7 @@ const requests = {
 	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
 	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
 	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+    
 };
+
+export default requests;
