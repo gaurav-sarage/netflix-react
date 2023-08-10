076404a72a31df799658fc3567160abdb37cb5df
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-10 22:38:46 +0530

    first base commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
+
+      <Counter />
       Welcome folks!
     </div>
   );
