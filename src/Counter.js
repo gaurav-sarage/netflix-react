f30cf3c2e0db12b8d8c382838adfecd2acc5d2a3
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-10 22:34:32 +0530

    initial file commit

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Counter() {
+    // Javascript code to handle the counter function
+    const [count, setCount] = useState(0);
+
+    return (
+    <div>
+      <h1>Counter</h1>
+      <p>You have clicked {count} times </p>
+      <button onClick={() => setCount(count+1)} ></button>
+    </div>
+  )
+}
+
+export default Counter
